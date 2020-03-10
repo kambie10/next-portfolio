727ad1c5372dfb6bf83104593efa1d24a8f57a02
David Maulana <davidmaulana10k@gmail.com>
2020-03-10 14:49:51 +0700

    make blog page

--- components/Layout.js
+++ components/Layout.js
@@ -23,6 +23,7 @@ const Layout = ({ children, title }) => {
         <Link href="/"><a>Home</a></Link>
         <Link href="/about"><a>About</a></Link>
         <Link href="/hireme"><a>Hireme</a></Link>
+        <Link href="/blog"><a>Blog</a></Link>
       </header>
       <h1>{title}</h1>
       {children}
---
+++ pages/post.js
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout'
+import { withRouter } from 'next/router'
+
+const Post = ({ router }) => (
+  <Layout title={router.query.title}>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore
+    magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur.
+    </p>
+  </Layout>
+)
+
+export default withRouter(Post)
