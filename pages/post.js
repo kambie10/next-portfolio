580d0bde99a9bb79046e194aac2dafd53f61954d
David Maulana <davidmaulana10k@gmail.com>
2020-03-10 15:39:01 +0700

    build now

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import { withRouter } from 'next/router'
 
 const Post = ({ router }) => (
   <Layout title={router.query.title}>
-    <p>
+    <p style={{ width: '80vw' }}>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore
     magna aliqua. Ut enim ad minim veniam, quis nostrud
